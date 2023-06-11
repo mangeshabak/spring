dd18eeb347e65e3c8bf11c9e3dd528dfed61c38a
mangeshabak <101352131+mangeshabak@users.noreply.github.com>
2023-06-11 13:43:58 +0530

    Spring Security and session Management

---
+++ Spring_Security_And_Session_Management/src/main/java/com/ems/Spring_Security_And_Session_Management.java
@@ -0,0 +1,13 @@
+package com.ems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring_Security_And_Session_Management {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring_Security_And_Session_Management.class, args);
+	}
+
+}
